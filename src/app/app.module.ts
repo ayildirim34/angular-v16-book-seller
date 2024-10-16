477db5fbb6c11d2054ff073434911248192f4a90
ayildirim34 <ayildirim3457@protonmail.com>
2024-10-17 01:50:56 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RegisterComponent } from './guest/register/register.component';
+import { HomeComponent } from './guest/home/home.component';
+import { LoginComponent } from './guest/login/login.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { UnauthorizedComponent } from './error/unauthorized/unauthorized.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    AdminComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
